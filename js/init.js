221a15ea62733e18d435f7369989f948599b13b6
Diogo Barbosa Silva Sousa <diogo.barbosa@gm5.com.br>
2019-12-20 00:19:44 -0300

    Configuração do template (Login)

--- js/init.js
+++ js/init.js
@@ -1,11 +1,21 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    //$('.sidenav').sidenav();
 
-    //$('select').formSelect();
+        $('.tabs').tabs();
 
-    M.AutoInit();
 
-  }); // end of document ready
+        $('.sidenav').sidenav();
+
+        $('select').formSelect();
+
+        $('input.autocomplete').autocomplete({
+            data: {
+                "Apple": null,
+                "Microsoft": null,
+                "Google": 'https://placehold.it/250x250'
+            },
+        });
+
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
